e5b5268cb2ce5ae81a3928e2057ac67743622d28
Cools <Cools@Michaels-RIG>
2021-11-29 12:02:48 -0600

    Basic Getters, setters, and construction beginnings of the Picture frame assignment. No picture things implemented yet.

---
+++ src/PictureData.java
@@ -0,0 +1,39 @@
+
+public class PictureData {
+	private String pic;
+	private String date;
+	private String desc;
+	
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public PictureData() {
+		pic = "";
+		date = "";
+		desc = "";
+	}
+	public PictureData(String pic, String date,String desc) {
+		setPic(pic);
+		setDate(date);
+		setDesc(desc);
+	}
+	@Override
+	public String toString() {
+		return String.format("%s\n%s\n%s",pic,date,desc);
+	}
+}
---
+++ src/PictureDataReader.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PictureDataReader {
+	public static PictureData createNewPic(String A,String B,String C) {
+		PictureData pic;
+		pic = new PictureData(A,B,C);
+		return pic;
+	}
+	public static ArrayList<PictureData> readQuestions(String fName) {
+		/*
+		 * Reads line for line through the text file and builds question by question putting the choices and answers inside.
+		 */
+		Scanner fsc;
+		ArrayList<PictureData> pics = new ArrayList<PictureData>();
+		String[] parts = null; 
+		String line,a = "",b = "",c = "";
+		PictureData fPics;
+		try {
+			fsc = new Scanner(new File(fName));
+			while (fsc.hasNextLine()) {
+				line = fsc.nextLine();
+				if (line.length() > 0) {
+					parts = line.split("\t");
+					a = parts[0];
+					b = parts[1];
+					c = parts[2];
+					fPics = createNewPic(a,b,c);
+					pics.add(fPics);
+					}
+			}
+			return pics;
+			}catch (Exception ex) {
+				System.out.println("Can't access that file");
+				return null;
+			}
+}
+}
